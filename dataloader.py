aa8db9773d7a47b1c6a71c971e5407a6cf9de312
rupali_khatun <rupali.khatun@live.com>
2020-03-29 00:03:33 +0100

    Added PyTorch CIFAR DS without Noise

--- dataloader.py
+++ dataloader.py
@@ -1,24 +1,42 @@
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import random_split
 import torchvision.transforms as transforms
 import random
 import numpy as np
 from PIL import Image
 import torchvision 
 
-train_root=r'/media/HDD_3TB2/rupali/Dataset/CIFAR10With4Class/Small_Train'
-test_root=r'/media/HDD_3TB2/rupali/Dataset/CIFAR10With4Class/Small_Test'
-val_root=r'/media/HDD_3TB2/rupali/Dataset/CIFAR10With4Class/Small_Val'
 
 
+class CustomCIFAR(Dataset):
+    def __init__(self, subset, transform=None, target_transform=None):
+        self.subset = subset
+        self.transform = transform
+        self.target_transform = target_transform
+        
+    def __getitem__(self, index):
+        img, target = self.subset[index]
+
+        if self.transform:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+        
+    def __len__(self):
+        return len(self.subset)
 
           
         
 class clothing_dataloader():  
-    def __init__(self, batch_size, num_workers, shuffle):
+    def __init__(self, batch_size, num_workers, shuffle, cifar_root=r'/media/HDD_3TB2/rupali/Dataset/CIFAR10'):
     
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.shuffle = shuffle
+        self.cifar_root = cifar_root
    
     def run(self):
         self.transform_train = transforms.Compose([
@@ -36,10 +54,19 @@ class clothing_dataloader():
                 transforms.ToTensor(),
                 transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225)),
             ])    
+
+        trainval_dataset = torchvision.datasets.CIFAR10(self.cifar_root, train=True, transform=None, target_transform=None, download=True)
+        test_dataset = torchvision.datasets.CIFAR10(self.cifar_root, train=False, transform=self.transform_test, target_transform=None, download=True)
                 
-        train_dataset = torchvision.datasets.ImageFolder(train_root, transform=self.transform_train)
-        test_dataset = torchvision.datasets.ImageFolder(test_root, transform=self.transform_test)
-        val_dataset = torchvision.datasets.ImageFolder(val_root, transform=self.transform_test)
+
+        lengths = [int(len(trainval_dataset)*0.8), int(len(trainval_dataset)*0.2)]
+        subsetA, subsetB = random_split(trainval_dataset, lengths)
+        train_dataset = CustomCIFAR(
+            subsetA, transform=self.transform_train)
+        )
+        val_dataset = CustomCIFAR(
+            subsetB, transform=self.transform_test)
+        )
         
         train_loader = DataLoader(
             dataset=train_dataset, 
